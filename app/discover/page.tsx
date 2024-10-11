d69599fd5f5f9d3f794b3f66d0b38f9750ba9735
riumat <matteoariu42@gmail.com>
2024-10-11 11:42:50 +0200

    changin mind

--- app/discover/page.tsx
+++ app/discover/page.tsx
@@ -4,9 +4,10 @@ import DropdownMenu from "@/components/DropdownMenu";
 export default async function DiscoverPage() {
   const genres = await getGenres();
   const watchProviders = await getWatchProviders();
+  const movieData = await getDiscoverMovies();
 
   return (
-    <div className="p-4">
+    <div className="flex-1 flex flex-col items-center gap-20 w-full">
       <h1 className="text-2xl font-bold mb-4">Discover</h1>
       <div className="space-y-4 flex gap-6">
         <DropdownMenu title="Genres" items={genres.map((genre: any) => ({ id: genre.id, name: genre.name }))} />
@@ -31,4 +32,10 @@ export async function getWatchProviders() {
   const res = await fetch(`${baseUrl}/watch/providers/movie?api_key=${process.env.TMDB_API_KEY}&watch_region=IT`);
   const data = await res.json();
   return data.results;
+}
+
+export async function getDiscoverMovies() {
+  const res = await fetch(`${baseUrl}/discover/movie?api_key=${process.env.TMDB_API_KEY}&page=1&sort_by=popularity.desc`);
+  const data = await res.json();
+  return data;
 }
